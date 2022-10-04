a03d8a10544fbaaf19e5d4e685610c6eab468dfa
Fineas Gavre <fineas.gavre@gmail.com>
2022-10-04 23:08:44 +0300

    add: ProgramState with executionStack / symbolTable / output & default constructor / getters/setters

---
+++ src/me/fineasgavre/apm/toylanguage/Domain/State/ProgramState.java
@@ -0,0 +1,48 @@
+package me.fineasgavre.apm.toylanguage.Domain.State;
+
+import me.fineasgavre.apm.toylanguage.Domain.ADTs.Interfaces.ITLList;
+import me.fineasgavre.apm.toylanguage.Domain.ADTs.Interfaces.ITLMap;
+import me.fineasgavre.apm.toylanguage.Domain.ADTs.Interfaces.ITLStack;
+import me.fineasgavre.apm.toylanguage.Domain.ADTs.TLList;
+import me.fineasgavre.apm.toylanguage.Domain.ADTs.TLMap;
+import me.fineasgavre.apm.toylanguage.Domain.ADTs.TLStack;
+import me.fineasgavre.apm.toylanguage.Domain.Statements.Interfaces.IStatement;
+import me.fineasgavre.apm.toylanguage.Domain.Values.Interfaces.IValue;
+
+public class ProgramState {
+    private ITLStack<IStatement> executionStack;
+    private ITLMap<String, IValue> symbolTable;
+    private ITLList<IValue> output;
+
+    public ProgramState(IStatement originalStatement) {
+        this.executionStack = new TLStack<>();
+        this.symbolTable = new TLMap<>();
+        this.output = new TLList<>();
+
+        this.executionStack.push(originalStatement);
+    }
+
+    public ITLStack<IStatement> getExecutionStack() {
+        return executionStack;
+    }
+
+    public void setExecutionStack(ITLStack<IStatement> executionStack) {
+        this.executionStack = executionStack;
+    }
+
+    public ITLMap<String, IValue> getSymbolTable() {
+        return symbolTable;
+    }
+
+    public void setSymbolTable(ITLMap<String, IValue> symbolTable) {
+        this.symbolTable = symbolTable;
+    }
+
+    public ITLList<IValue> getOutput() {
+        return output;
+    }
+
+    public void setOutput(ITLList<IValue> output) {
+        this.output = output;
+    }
+}
